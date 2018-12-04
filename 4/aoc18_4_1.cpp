2f402d91804424349aaa709c083e200a35186aed
Raihan Kibria <rkibria@users.noreply.github.com>
2018-12-04 22:55:11 +0000

    Parse 4.1

--- 4/aoc18_4_1.cpp
+++ 4/aoc18_4_1.cpp
@@ -7,15 +7,30 @@
 #include <memory>
 using namespace std;
 
+enum class GuardState { begin, sleep, wake };
+
+ostream& operator<<( ostream& os, const GuardState& s )
+{
+	switch( s )
+	{
+		case GuardState::begin: os << "begins shift"; break;
+		case GuardState::sleep: os << "falls asleep"; break;
+		case GuardState::wake: os << "wakes up"; break;
+		default: assert( false ); break;
+	}
+	return os;
+}
+
 struct Record
 {
-	unsigned int month, day, hour, minute;
+	unsigned int month, day, hour, minute, guard;
+	GuardState state;
 	string event;
 };
 
 ostream& operator<<( ostream& os, const Record& r )
 {
-	os << "(1518-" << r.month << "-" << r.day << " " << r.hour << ":" << r.minute << " '" << r.event.c_str() <<  "')";
+	os << "(1518-" << r.month << "-" << r.day << " " << r.hour << ":" << r.minute << " - guard " << r.guard << " " << r.state <<  ")";
 	return os;
 }
 
@@ -47,6 +62,8 @@ int main( int argc, char* argv[] )
 			auto rec = make_unique< Record >();
 			rec->month = stoul( token.substr( 6, 8 ) );
 			rec->day = stoul( token.substr( 9, 11 ) );
+			assert( rec->month >= 1 && rec->month <= 12 );
+			assert( rec->day >= 1 && rec->day <= 31 );
 
 			iss >> token; // "00:42]"
 			assert( token[ 2 ] == ':' );
@@ -54,8 +71,30 @@ int main( int argc, char* argv[] )
 
 			rec->hour = stoul( token.substr( 0, 2 ) );
 			rec->minute = stoul( token.substr( 3, 5 ) );
+			assert( rec->hour >= 0 && rec->hour <= 23 );
+			assert( rec->minute >= 0 && rec->minute <= 59 );
 
-			cout << *rec << endl;
+			iss >> token;
+			assert( token == "wakes" || token == "falls" || token == "Guard" );
+
+			if( token == "wakes" )
+			{
+				rec->state = GuardState::wake;
+			}
+			else if( token == "falls" )
+			{
+				rec->state = GuardState::sleep;
+			}
+			else if( token == "Guard" )
+			{
+				rec->state = GuardState::begin;
+				iss >> token; // "#2617"
+				assert( token[ 0 ] == '#' );
+
+				rec->guard = stoul( token.substr( 1 ) );
+				assert( rec->guard > 0 );
+			}
+
 			records.emplace_back( move( rec ) );
 		}
 		file.close();
