cd39e14039a7ae12e3eb7cc12d661156f33d95df
Raihan Kibria <rkibria@users.noreply.github.com>
2018-12-07 17:39:12 +0000

    5.2 Solution

--- 5/aoc18_5.cpp
+++ 5/aoc18_5.cpp
@@ -4,31 +4,12 @@
 #include <ctype.h>
 #include <string>
 #include <cassert>
+#include <algorithm>
 using namespace std;
 
-int main( int argc, char* argv[] )
+string collapse_polymer( const string& original )
 {
-	if( argc < 2 )
-	{
-		cout << "Usage: " << argv[ 0 ] << " <input file>" << endl;
-		return -1;
-	}
-
-	ifstream file( argv[ 1 ] );
-	string polymer;
-	if( file.is_open() )
-	{
-		getline( file, polymer );
-		assert( !polymer.empty() );
-	}
-	else
-	{
-		cerr << "ERROR: could not open file" << endl;
-		return -1;
-	}
-
-	cout << "polymer length: " << polymer.size() << endl;
-
+	string polymer = original;
 	string result;
 	result.reserve( polymer.size() );
 
@@ -69,10 +50,54 @@ int main( int argc, char* argv[] )
 		{
 			polymer = result;
 			result.clear();
-			cout << "reacting..." << endl;
 		}
 	}
-	cout << "1) polymer length after reaction: " << result.size() << endl;
+
+	return result;
+}
+
+int main( int argc, char* argv[] )
+{
+	if( argc < 2 )
+	{
+		cout << "Usage: " << argv[ 0 ] << " <input file>" << endl;
+		return -1;
+	}
+
+	ifstream file( argv[ 1 ] );
+	string polymer;
+	if( file.is_open() )
+	{
+		getline( file, polymer );
+		assert( !polymer.empty() );
+	}
+	else
+	{
+		cerr << "ERROR: could not open file" << endl;
+		return -1;
+	}
+
+	cout << "polymer length: " << polymer.size() << endl;
+
+	string result_1 = collapse_polymer( polymer );
+	cout << "1) polymer length after reaction: " << result_1.size() << endl;
+
+	size_t lowest_length = 0xffffffff;
+	char best_unit = 0;
+	for( char unit = 'a'; unit <= 'z'; ++unit )
+	{
+		string p = polymer;
+		p.erase( remove_if( p.begin(), p.end(), [ unit ]( char x ) { return x == unit || x == toupper( unit ); } ),
+			p.end() );
+		string r = collapse_polymer( p );
+		cout << "remove unit " << unit << ": polymer length " << r.size() << endl;
+		if( r.size() < lowest_length )
+		{
+			lowest_length = r.size();
+			best_unit = unit;
+		}
+	}
+	cout << "2) polymer lowest length " << lowest_length << " when removing unit type " << best_unit << endl;
 
 	return 0;
 }
