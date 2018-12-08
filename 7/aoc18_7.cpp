d0c9b929cf9f1c9894f6c975aae4426dbe401136
Raihan Kibria <rkibria@users.noreply.github.com>
2018-12-08 23:39:29 +0000

    7.1 DFS

--- 7/aoc18_7.cpp
+++ 7/aoc18_7.cpp
@@ -7,6 +7,34 @@
 #include <unordered_map>
 using namespace std;
 
+using StepId = char;
+using Instruction = pair< StepId, StepId >;
+using AdjacencyElements = vector< StepId >;
+using AdjacencyList = unordered_map< StepId, AdjacencyElements >;
+
+struct StepData
+{
+	bool visited;
+	int depth;
+};
+
+using StepMap = unordered_map< StepId, StepData >;
+
+void dfs( AdjacencyList& lst, StepId step, StepMap& smp )
+{
+	cout << "dfs: " << step << endl;
+	smp[ step ].visited = true;
+
+	for( const auto prev_step : lst[ step ] )
+	{
+		if( !smp[ prev_step ].visited )
+		{
+			cout << step << " -> " << prev_step << endl;
+			dfs( lst, prev_step, smp );
+		}
+	}
+}
+
 int main( int argc, char* argv[] )
 {
 	if( argc < 2 )
@@ -15,8 +43,6 @@ int main( int argc, char* argv[] )
 		return -1;
 	}
 
-	using NodeId = char;
-	using Instruction = pair< NodeId, NodeId >;
 	vector< Instruction > instructions;
 
 	ifstream file( argv[ 1 ] );
@@ -29,13 +55,13 @@ int main( int argc, char* argv[] )
 			ifs >> token;
 			assert( token == "Step" );
 
-			NodeId step_1;
+			StepId step_1;
 			ifs >> step_1;
 
 			for( int i = 0; i < 5; ++i )
 				ifs >> token;
 
-			NodeId step_2;
+			StepId step_2;
 			ifs >> step_2;
 
 			instructions.emplace_back( Instruction( step_1, step_2 ) );
@@ -50,40 +76,47 @@ int main( int argc, char* argv[] )
 
 	cout << "Instructions: " << instructions.size() << endl;
 
-	using AdjacencyElements = vector< NodeId >;
-	using AdjacencyList = unordered_map< NodeId, AdjacencyElements >;
-
-	auto initialize_entries = []( AdjacencyList& lst, NodeId src, NodeId dst ) {
+	auto initialize_adjacency_list = []( AdjacencyList& lst, StepId src, StepId dst ) {
 			if( lst.find( src ) == lst.end() )
 				lst[ src ] = AdjacencyElements();
 			if( lst.find( dst ) == lst.end() )
 				lst[ dst ] = AdjacencyElements();
 		};
 
+	auto initialize_step_map = []( StepMap& smp, StepId step ) {
+			if( smp.find( step ) == smp.end() )
+				smp[ step ] = StepData{ false, 0 };
+		};
+
+	StepMap smp;
 	AdjacencyList incoming, outgoing;
 	for( const auto& instr : instructions )
 	{
 		const auto src = instr.first;
 		const auto dst = instr.second;
 
-		initialize_entries( incoming, src, dst );
-		initialize_entries( outgoing, src, dst );
+		initialize_step_map( smp, src );
+		initialize_step_map( smp, dst );
 
+		initialize_adjacency_list( incoming, src, dst );
+		initialize_adjacency_list( outgoing, src, dst );
 		incoming[ dst ].emplace_back( src );
 		outgoing[ src ].emplace_back( dst );
 	}
 
-	for( auto itr : incoming )
-	{
-		if( itr.second.empty() )
-			cout << "start node " << itr.first << endl;
-	}
-
+	StepId final_step{ 0 };
 	for( auto itr : outgoing )
 	{
 		if( itr.second.empty() )
-			cout << "end node " << itr.first << endl;
+		{
+			final_step = itr.first;
+			cout << "final step is " << final_step << endl;
+			break;
+		}
 	}
+	assert( final_step != 0 );
+
+	dfs( incoming, final_step, smp );
 
 	return 0;
 }
