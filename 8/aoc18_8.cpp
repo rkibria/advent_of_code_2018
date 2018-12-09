261ee61b6fd6387d570e0be9268976e7b4ac15a8
Raihan Kibria <rkibria@users.noreply.github.com>
2018-12-09 19:31:38 +0000

    8.1 Solution

--- 8/aoc18_8.cpp
+++ 8/aoc18_8.cpp
@@ -6,6 +6,41 @@
 #include <sstream>
 using namespace std;
 
+using InputVector = vector< size_t >;
+
+static size_t n_children_found = 0;
+static size_t metadata_sum = 0;
+
+size_t sum_all_metadata( const InputVector& inputs, size_t start_index )
+{
+	const size_t n_children = inputs[ start_index ];
+	const size_t n_metadata = inputs[ start_index + 1 ];
+
+	const size_t node_id = n_children_found;
+	++n_children_found;
+
+	size_t node_length = 2;
+	node_length += n_metadata;
+
+	size_t children_length = 0;
+	for( size_t i = 0; i < n_children; ++i )
+	{
+		children_length += sum_all_metadata( inputs, start_index + 2 + children_length );
+	}
+
+	if( n_metadata > 0 )
+	{
+		for( size_t i = 0; i < n_metadata; ++i )
+		{
+			metadata_sum += inputs[ start_index + 2 + children_length + i ];
+		}
+	}
+
+	node_length += children_length;
+
+	return node_length;
+}
+
 int main( int argc, char * argv[] )
 {
 	if( argc < 2 )
@@ -14,7 +49,7 @@ int main( int argc, char * argv[] )
 		return -1;
 	}
 
-	vector< int > input;
+	InputVector inputs;
 
 	ifstream file( argv[1] );
 	if( file.is_open() )
@@ -27,13 +62,19 @@ int main( int argc, char * argv[] )
 		while( !iss.eof() )
 		{
 			iss >> token;
-			input.emplace_back( stoi( token) );
+			inputs.emplace_back( stoul( token) );
 		}
 
 		file.close();
 	}
 
-	cout << "input length: " << input.size() << endl;
+	cout << "input length: " << inputs.size() << endl;
+
+	// PART 1
+	const size_t inputs_read = sum_all_metadata( inputs, 0 );
+	assert( inputs_read == inputs.size() );
+
+	cout << "1) sum of metadata: " << metadata_sum << endl;
 
 	return 0;
 }
