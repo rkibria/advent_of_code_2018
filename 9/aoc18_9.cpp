eb2ff9a4350bd792a37c7d1d41e0cad01d7acce2
Raihan Kibria <rkibria@users.noreply.github.com>
2018-12-12 07:46:50 +0000

    9.1

--- 9/aoc18_9.cpp
+++ 9/aoc18_9.cpp
@@ -5,23 +5,20 @@ using namespace std;
 
 int main( int argc, char * argv[] )
 {
+	const size_t num_players = 9;
+	const size_t last_marble = 24;
+
 	using CircleContainer = vector< int >;
 
 	CircleContainer circle( 1 );
+	vector< size_t > scores( num_players );
 
-	int cur_m = 1;
-	int cur_i = 0;
+	size_t cur_m = 1;
+	size_t cur_i = 0;
+	size_t cur_p = 0;
 
-	for( int i = 0; i < 25; ++i )
+	for( int i = 0; i < last_marble + 1; ++i )
 	{
-		for( size_t j = 0; j < circle.size(); ++j )
-		{
-			if( j == cur_i )
-				cout << ".";
-			cout << circle[ j ] << " ";
-		}
-		cout << endl << endl;
-
 		const size_t one_clockwise = ( cur_i + 1 ) % circle.size();
 		if( one_clockwise == circle.size() - 1 )
 		{
@@ -36,6 +33,17 @@ int main( int argc, char * argv[] )
 		}
 
 		++cur_m;
+
+		cout << cur_p + 1 << ": ";
+		for( size_t j = 0; j < circle.size(); ++j )
+		{
+			if( j == cur_i )
+				cout << ".";
+			cout << circle[ j ] << " ";
+		}
+		cout << endl << endl;
+
+		cur_p = ( cur_p + 1 ) % num_players;
 	}
 
 	return 0;
