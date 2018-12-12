33d1ac2c5e08f92b72730cd5af3b6354eb193d26
Raihan Kibria <rkibria@users.noreply.github.com>
2018-12-12 21:53:06 +0000

    9.2

--- 9/aoc18_9_2.cpp
+++ 9/aoc18_9_2.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 int main( int argc, char * argv[] )
 {
-	const size_t num_players = 405;
-	const size_t last_marble = 71700;
+	const size_t num_players = 405; // 9; // 10405;
+	const size_t last_marble = 71700 * 100; // 25; // 71700; // * 100;
 
 	cout << "number of players: " << num_players << endl;
 	cout << "last marble: " << last_marble << endl;
@@ -20,15 +20,15 @@ int main( int argc, char * argv[] )
 
 	size_t cur_m = 1;
 	size_t cur_i = 0;
+	auto cur_i_itr = circle.begin();
 	size_t cur_p = 0;
 
 	auto get_iterator = [ &circle ]( size_t i ) {
 			assert( i < circle.size() );
-			return circle.begin();
-		};
-
-	auto get_value = [ get_iterator, &circle ]( size_t i ) {
-			return *get_iterator( i );
+			auto itr = circle.begin();
+			for( size_t j = 0; j < i; ++j )
+				++itr;
+			return itr;
 		};
 
 	for( size_t i = 0; i < last_marble + 1; ++i )
@@ -48,27 +48,61 @@ int main( int argc, char * argv[] )
 				other_m_i = circle.size() - ( 7 - cur_i );
 			}
 
-			scores[ cur_p ] += get_value( other_m_i );
-			circle.erase( get_iterator( other_m_i ) );
+			auto other_m_i_itr = get_iterator( other_m_i );
+
+			scores[ cur_p ] += *other_m_i_itr;
+
+			other_m_i_itr = circle.erase( other_m_i_itr );
 			cur_i = other_m_i;
+			cur_i_itr = other_m_i_itr;
+			// cout << "!!!" << endl;
 		}
 		else
 		{
 			const size_t one_clockwise = ( cur_i + 1 ) % circle.size();
 			if( one_clockwise == circle.size() - 1 )
 			{
+				// cout << "@@@" << endl;
 				circle.insert( circle.end(), cur_m );
 				cur_i = circle.size() - 1;
+				cur_i_itr = circle.end();
 			}
 			else
 			{
+				// cout << "+++" << endl;
+				++cur_i_itr;
+				if( cur_i_itr == circle.end() )
+				{
+					cur_i_itr = circle.begin();
+					// cout << "+++R1" << endl;
+				}
+
+				++cur_i_itr;
+				if( cur_i_itr == circle.end() )
+				{
+					cur_i_itr = circle.begin();
+					// cout << "+++R2" << endl;
+				}
+
 				const size_t two_clockwise = ( one_clockwise + 1 ) % circle.size();
-				circle.insert( get_iterator( two_clockwise ), cur_m );
+
+				cur_i_itr = circle.insert( cur_i_itr, cur_m );
 				cur_i = two_clockwise;
 			}
 		}
 
 		++cur_m;
+
+		// cout << cur_p + 1 << ": ";
+		// auto itr_j = circle.begin();
+		// for( size_t j = 0; j < circle.size(); ++j )
+		// {
+			// if( j == cur_i )
+				// cout << ".";
+			// cout << *itr_j << " ";
+			// itr_j++;
+		// }
+		// cout << endl << endl;
 
 		cur_p = ( cur_p + 1 ) % num_players;
 	}
