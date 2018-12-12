022cf4865ae04b5f1d18e56b16b7cbfa9eae63c6
Raihan Kibria <rkibria@users.noreply.github.com>
2018-12-12 23:30:07 +0000

    9.2 Solution

--- 9/aoc18_9_2.cpp
+++ 9/aoc18_9_2.cpp
@@ -26,8 +26,6 @@ int main( int argc, char * argv[] )
 	auto get_iterator = [ &circle ]( size_t i ) {
 			assert( i < circle.size() );
 
-			// cout << i << " (" << circle.size() << ") " << (static_cast<double>(i) / circle.size()) << endl;
-
 			auto itr = circle.begin();
 
 			if( i < circle.size() / 2 )
@@ -50,9 +48,6 @@ int main( int argc, char * argv[] )
 
 	for( size_t i = 0; i < last_marble + 1; ++i )
 	{
-		if( i % 10000 == 0 )
-			cout << i << "..." << endl;
-
 		if( cur_m % 23 == 0 )
 		{
 			scores[ cur_p ] += cur_m;
@@ -63,10 +58,7 @@ int main( int argc, char * argv[] )
 			size_t other_m_i{ 0 };
 			if( cur_i >= cclkw_distance )
 			{
-				// cout << cur_i << endl;
 				other_m_i = cur_i - cclkw_distance;
-
-				// other_m_i_itr = get_iterator( other_m_i );
 
 				other_m_i_itr = cur_i_itr;
 				for( size_t k = 0; k < cclkw_distance; ++k )
@@ -91,7 +83,6 @@ int main( int argc, char * argv[] )
 			const size_t one_clockwise = ( cur_i + 1 ) % circle.size();
 			if( one_clockwise == circle.size() - 1 )
 			{
-				cout << "@@@" << endl;
 				circle.insert( circle.end(), cur_m );
 				cur_i = circle.size() - 1;
 				cur_i_itr = circle.end();
@@ -101,12 +92,10 @@ int main( int argc, char * argv[] )
 			{
 				const size_t two_clockwise = ( one_clockwise + 1 ) % circle.size();
 
-				// cout << "+++" << endl;
 				++cur_i_itr;
 				if( cur_i_itr == circle.end() )
 				{
 					cur_i_itr = circle.begin();
-					cout << "+++R1 " << two_clockwise << endl;
 				}
 
 				++cur_i_itr;
@@ -114,7 +103,6 @@ int main( int argc, char * argv[] )
 				{
 					cur_i_itr = circle.begin();
 					++cur_i_itr;
-					cout << "+++R2 " << two_clockwise << endl;
 				}
 
 				cur_i_itr = circle.insert( cur_i_itr, cur_m );
@@ -123,17 +111,6 @@ int main( int argc, char * argv[] )
 		}
 
 		++cur_m;
-
-		// cout << cur_p + 1 << ": ";
-		// auto itr_j = circle.begin();
-		// for( size_t j = 0; j < circle.size(); ++j )
-		// {
-			// if( j == cur_i )
-				// cout << ".";
-			// cout << *itr_j << " ";
-			// itr_j++;
-		// }
-		// cout << endl << endl;
 
 		cur_p = ( cur_p + 1 ) % num_players;
 	}
