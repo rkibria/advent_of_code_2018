5131612e67b7d8fcab12055317a413ae263db057
Raihan Kibria <rkibria@users.noreply.github.com>
2018-12-12 07:37:18 +0000

    9.1 Refactor

--- 9/aoc18_9.cpp
+++ 9/aoc18_9.cpp
@@ -7,35 +7,13 @@ int main( int argc, char * argv[] )
 {
 	using CircleContainer = vector< int >;
 
-	CircleContainer circle;
+	CircleContainer circle( 1 );
 
-	int cur_m = 0;
+	int cur_m = 1;
 	int cur_i = 0;
 
 	for( int i = 0; i < 25; ++i )
 	{
-		if( circle.empty() )
-		{
-			circle.insert( circle.begin(), cur_m );
-		}
-		else
-		{
-			const size_t one_clockwise = ( cur_i + 1 ) % circle.size();
-			if( one_clockwise == circle.size() - 1 )
-			{
-				circle.insert( circle.end(), cur_m );
-				cur_i = circle.size() - 1;
-			}
-			else
-			{
-				const size_t two_clockwise = ( one_clockwise + 1 ) % circle.size();
-				circle.insert( circle.begin() + two_clockwise, cur_m );
-				cur_i = two_clockwise;
-			}
-		}
-
-		++cur_m;
-
 		for( size_t j = 0; j < circle.size(); ++j )
 		{
 			if( j == cur_i )
@@ -43,6 +21,21 @@ int main( int argc, char * argv[] )
 			cout << circle[ j ] << " ";
 		}
 		cout << endl << endl;
+
+		const size_t one_clockwise = ( cur_i + 1 ) % circle.size();
+		if( one_clockwise == circle.size() - 1 )
+		{
+			circle.insert( circle.end(), cur_m );
+			cur_i = circle.size() - 1;
+		}
+		else
+		{
+			const size_t two_clockwise = ( one_clockwise + 1 ) % circle.size();
+			circle.insert( circle.begin() + two_clockwise, cur_m );
+			cur_i = two_clockwise;
+		}
+
+		++cur_m;
 	}
 
 	return 0;
