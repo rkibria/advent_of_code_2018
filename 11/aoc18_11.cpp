55165a5bae1b20d874513168b9eb557524d6bba4
Raihan Kibria <rkibria@users.noreply.github.com>
2018-12-15 23:04:22 +0000

    11.1 Solution

--- 11/aoc18_11.cpp
+++ 11/aoc18_11.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <cassert>
 #include <vector>
-#include <sstream>
 #include <algorithm>
-#include <memory>
 #include <limits>
-#include <set>
 using namespace std;
 
 int main( int argc, char* argv[] )
 {
-	const int serial_number = 8;
-
 	using RowVector = vector < int >;
 	vector< RowVector > grid;
 
@@ -27,7 +20,7 @@ int main( int argc, char* argv[] )
 
 	auto powerlevel = [ &hundreds_digit ]( int sn, size_t x, size_t y ) {
 			const auto rack_id = static_cast< int >( x ) + 10;
-			int lvl = rack_id * static_cast< int >( y );
+			auto lvl = rack_id * static_cast< int >( y );
 			lvl += sn;
 			lvl *= rack_id;
 			lvl = hundreds_digit( lvl );
@@ -40,9 +33,31 @@ int main( int argc, char* argv[] )
 	assert( powerlevel( 39, 217, 196 ) == 0 );
 	assert( powerlevel( 71, 101, 153 ) == 4 );
 
+	const int serial_number = 8199;
 	for( size_t y = 0 ; y < grid_size; ++y )
 		for( size_t x = 0 ; x < grid_size; ++x )
 			grid[ y ][ x ] = powerlevel( serial_number, x, y );
+
+	int max_sum = numeric_limits< int >::min();
+	pair< size_t, size_t > max_pos{ 0, 0 };
+	for( size_t y = 0 ; y < grid_size - 3; ++y )
+		for( size_t x = 0 ; x < grid_size - 3; ++x )
+		{
+			int sum = 0;
+			for( size_t i = 0 ; i < 3; ++i )
+				for( size_t j = 0 ; j < 3; ++j )
+					sum += grid[ y + i ][ x + j ];
+
+			if( sum > max_sum )
+			{
+				max_sum = sum;
+				max_pos.first = x;
+				max_pos.second = y;
+			}
+		}
+
+	cout << "1) largest 3x3 square position for serial number " << serial_number << ": "
+		<< max_pos.first << "," << max_pos.second << " (total power " << max_sum << ")" << endl; 
 
 	return 0;
 }
