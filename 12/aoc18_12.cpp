ab9a20fecc81ef75fc0dc44d47dc39fef3e5f846
Raihan Kibria <rkibria@users.noreply.github.com>
2018-12-22 23:19:15 +0100

    12.1 Parse

--- 12/aoc18_12.cpp
+++ 12/aoc18_12.cpp
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <vector>
 #include <sstream>
+#include <map>
 using namespace std;
 
 int main( int argc, char* argv[] )
@@ -13,6 +14,12 @@ int main( int argc, char* argv[] )
 		cout << "Usage: " << argv[ 0 ] << " <input file>" << endl;
 		return -1;
 	}
+
+	const size_t zero_pos = 20;
+	const size_t note_size = 5;
+	string state( zero_pos, '.' );
+
+	map< string, char > notes;
 
 	ifstream file( argv[ 1 ] );
 	if( file.is_open() )
@@ -29,18 +36,24 @@ int main( int argc, char* argv[] )
 		assert( token == "state:" );
 
 		ifs >> token;
+		state.append( token );
 
 		while( getline( file, line ) )
 		{
 			if( !line.empty() )
 			{
+				string note;
 				istringstream ifs( line );
-				ifs >> token;
+				ifs >> note;
+				assert( note.size() == note_size );
 
 				ifs >> token;
 				assert( token == "=>" );
 
 				ifs >> token;
+				assert( token.length() == 1 );
+
+				notes[ note ] = token[ 0 ];
 			}
 		}
 		file.close();
@@ -51,6 +64,21 @@ int main( int argc, char* argv[] )
 		return -1;
 	}
 
+	state.append( zero_pos, '.' );
+	cout << "initial state: " << state << endl;
+	cout << "notes: " << notes.size() << endl;
+
+	// PART 1
+	for( size_t i = 0; i < state.length() - note_size; ++i )
+	{
+		const string stretch = state.substr( i, note_size );
+		cout << stretch << endl;
+		if( notes.find( stretch ) != notes.end() )
+		{
+			cout << "=> " << notes[ stretch ] << endl;
+		}
+	}
+	
 	return 0;
 }
 
