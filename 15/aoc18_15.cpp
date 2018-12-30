2bfe3c498c459b8f4821025c544796ab9341a96e
Raihan Kibria <rkibria@users.noreply.github.com>
2018-12-30 12:33:17 +0100

    15.1 Start

---
+++ 15/aoc18_15.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+#include <fstream>
+#include <memory>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	if( argc < 2 ) {
+		cout << "Usage: " << argv[0] << " <input file>" << endl;
+		return -1;
+	}
+
+	ifstream file(argv[1]);
+	if(file.is_open()) {
+		string line;
+		while(getline(file, line)) {
+			assert(!line.empty());
+			for(auto c : line) {
+				switch(c) {
+				case '.':
+				case '#':
+					break;
+				case 'E':
+					cout << "E" << endl;
+					break;
+				case 'G':
+					cout << "G" << endl;
+					break;
+				default:
+					assert(false);
+					break;
+				}
+			}
+		}
+		file.close();
+	}
+	else{
+		cerr << "ERROR: could not open file" << endl;
+		return -1;
+	}
+
+	return 0;
+}
+
